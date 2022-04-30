f8af337e3c73fe3db42f5dfd5b67da1745bb6243
Alex <alexanderjukes@gmail.com>
2022-04-30 17:47:32 +0100

    Add loading state while we fetch patient data

--- src/components/PatientInformation/PatientInformation.tsx
+++ src/components/PatientInformation/PatientInformation.tsx
@@ -6,19 +6,25 @@ const PATIENT_DATA_ENDPOINT =
   "https://61ba219448df2f0017e5a929.mockapi.io/api/patients";
 
 const PatientInformation: React.FC = () => {
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
   const [patientData, setPatientData] = React.useState<Patient[]>([]);
 
   React.useEffect(() => {
     const getPatientData = async () => {
       const fetchedPatientData = await fetchPatientData(PATIENT_DATA_ENDPOINT);
       setPatientData(fetchedPatientData);
+      setIsLoading(false);
     };
     getPatientData();
   }, []);
   return (
     <>
       <h1>Patient Information</h1>
-      <PatientDataTable patients={patientData} />
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <PatientDataTable patients={patientData} />
+      )}
     </>
   );
 };
--- src/components/PatientInformation/test/PatientInformation.test.tsx
+++ src/components/PatientInformation/test/PatientInformation.test.tsx
@@ -1,9 +1,49 @@
 import { screen, render } from "@testing-library/react";
 import { PatientInformation } from "..";
+import { fetchPatientData } from "../../../api/fetchPatientData";
+import { PatientDataTable } from "../../PatientDataTable";
+
+jest.mock("../../../api/fetchPatientData");
+jest.mock("../../PatientDataTable");
 
 describe("PatientInformation", () => {
-  it("renders", () => {
+  const mockPatientData = [
+    {
+      firstName: "Gelato",
+      lastName: "Al Cioccolato",
+      vaccineDate: 1637877061,
+      vaccineType: "Pfizer",
+      nhsNumber: "4584318425",
+      id: "1",
+    },
+  ];
+
+  beforeEach(() => {
+    (PatientDataTable as jest.Mock).mockImplementation(() => (
+      <div data-testid="mock-patient-data-table" />
+    ));
+  });
+
+  it("renders", async () => {
     render(<PatientInformation />);
-    expect(screen.getByText("Patient Information")).toBeInTheDocument();
+    expect(await screen.findByText("Patient Information")).toBeInTheDocument();
+  });
+
+  it("shows a loading message until data has been fetched", async () => {
+    render(<PatientInformation />);
+    expect(await screen.findByText("Loading...")).toBeInTheDocument();
+    expect(
+      screen.queryByTestId("mock-patient-data-table")
+    ).not.toBeInTheDocument();
+  });
+
+  it("renders the PatientDataTable with correct info once data has been fetched", async () => {
+    (fetchPatientData as jest.Mock).mockResolvedValueOnce({
+      data: mockPatientData,
+    });
+    render(<PatientInformation />);
+    expect(
+      await screen.findByTestId("mock-patient-data-table")
+    ).toBeInTheDocument();
   });
 });
