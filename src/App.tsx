37a6bf4c61bb8135906bec0692f74df0393cc110
Alex <alexanderjukes@gmail.com>
2022-04-30 15:27:08 +0100

    First commit
    - Create react app
    - Update header

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Accurx Dashboard</header>
+    </div>
+  );
+}
+
+export default App;
